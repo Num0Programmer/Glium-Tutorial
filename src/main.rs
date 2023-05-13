d3f6277ce68402ff08060fb4fbaf792249ca0d8b
Num0Programmer <asajh4@gmail.com>
2023-05-13 14:54:27 -0700

    Finished with "Opening a window" piece of the tutorial by adding a drawing surface, making it a solid color, then finally destroying the surface and copying it to the window.

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,22 @@ extern crate glium;
 #[allow(unused_variables)]
 fn main()
 {
-    use glium::glutin;
+    // trait imports
+    use glium::{glutin, Surface};
 
-    // put together Glium Display
+    // construct Glium Display
     let mut event_loop = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new();
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
+    // run event loop
     event_loop.run(move |ev, _, control_flow|
     {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap(); // destroy frame and copy surface to screen
+
         let next_frame_time = std::time::Instant::now()
             + std::time::Duration::from_nanos(16_666_67);
         *control_flow = glutin::event_loop::ControlFlow
