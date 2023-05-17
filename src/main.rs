e93b958313f3af5806a294f9a00e3bb020552c36
Num0Programmer <asajh4@gmail.com>
2023-05-17 16:50:58 -0700

    Expounded upon vertex shader and concept of matrices for representation in order to create a simple camera which can be placed around the scene to view the teapot from different positions and angles.

--- src/main.rs
+++ src/main.rs
@@ -27,20 +27,22 @@ fn main()
 
     // GLSL code for a vertex shader
     let vertex_shader_src = r#"
-        #version 150 // GLSL version which supports trig functions
+        #version 140
 
         in vec3 position;
         in vec3 normal;
 
-        out vec3 v_normal; // used to get normal data to fragment shader
+        out vec3 v_normal;
 
         uniform mat4 perspective;
-        uniform mat4 matrix;
+        uniform mat4 view;
+        uniform mat4 model;
 
         void main()
         {
-            v_normal = transpose(inverse(mat3(matrix))) * normal;
-            gl_Position = perspective * matrix * vec4(position, 1.0);
+            mat4 modelview = view * model;
+            v_normal = transpose(inverse(mat3(modelview))) * normal;
+            gl_Position = perspective * modelview * vec4(position, 1.0);
         }
     "#;
 
@@ -100,6 +102,11 @@ fn main()
         // reset display color and depth buffer to blue and 1, respectively
         target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
 
+        let view = view_matrix(
+            &[2.0, -1.0, 1.0],
+            &[-2.0, 1.0, 1.0],
+            &[0.0, 1.0, 0.0]
+        );
         let perspective = {
             let (width, height) = target.get_dimensions();
             let aspect_ratio = height as f32 / width as f32;
@@ -118,12 +125,13 @@ fn main()
         };
 
         let light = [-1.0, 0.4, 0.9f32];
-        let matrix = [
+        let model = [
             [0.01, 0.0, 0.0, 0.0],
             [0.0, 0.01, 0.0, 0.0],
             [0.0, 0.0, 0.01, 0.0],
             [0.0, 0.0, 2.0, 1.0f32]
         ];
+
         // needed to tell backend what to do with a list of 'possible'
         // operations; needed because depth test and buffer handling happen on
         // hardware
@@ -149,11 +157,57 @@ fn main()
             &indices,
             &program,
             &uniform! {
-                matrix: matrix, perspective: perspective, u_light: light
+                model: model, view: view,
+                perspective: perspective, u_light: light
             },
             &params
         ).unwrap();
         target.finish().unwrap();
     });
+}
+
+// position: position of the camera in the scene
+// direction: direction camera is facing in scene coords
+// up: represents the direction in scene coords of the top of the screen
+fn view_matrix(
+    position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]
+) -> [[f32; 4]; 4]
+{
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    let s = [
+        up[1] * f[2] - up[2] * f[1],
+        up[2] * f[0] - up[0] * f[2],
+        up[0] * f[1] - up[1] * f[0]
+    ];
+
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    let u = [
+        f[1] * s_norm[2] - f[2] * s_norm[1],
+        f[2] * s_norm[0] - f[0] * s_norm[2],
+        f[0] * s_norm[1] - f[1] * s_norm[0]
+    ];
+
+    let p = [
+        -position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+        -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+        -position[0] * f[0] - position[1] * f[1] - position[2] * f[2]
+    ];
+    [
+        [s_norm[0], u[0], f[0], 0.0],
+        [s_norm[1], u[1], f[1], 0.0],
+        [s_norm[2], u[2], f[2], 0.0],
+        [p[0], p[1], p[2], 1.0]
+    ]
 }
 
